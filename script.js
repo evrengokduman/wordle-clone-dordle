1d3a2e493ce7c662d8430432cac7584256889f76
evrengokduman <sukruevrengokduman@gmail.com>
2023-05-07 12:28:51 +0300

    Files Setup

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+import { WORDS } from "./words.js";
 const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = []; // array of letters in current guess
+let nextLetter = 0; // index of next letter to guess
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]; // random word from WORDS array
+console.log(rightGuessString);
