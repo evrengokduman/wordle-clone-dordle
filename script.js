59dbfb1b8de1683ee35a2d0c42587ef231d92245
evrengokduman <sukruevrengokduman@gmail.com>
2023-05-08 16:03:25 +0300

    added comments

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ let nextLetter = 0; // index of next letter to guess
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]; // random word from WORDS array
 console.log(rightGuessString);
 
+//generates rows, gives the class
 function initBoard() {
   let board = document.getElementById("game-board");
-
+  //this loop, each time it runs, it's gonna create a row with a class of letterRow
   for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
     console.log("first loop");
     let row = document.createElement("div");
     row.className = "letter-row";
-
+    //loop 5 times and creates boxes and it's gonna nest each of those boxes inside of row
     for (let j = 0; j < 5; j++) {
       console.log("second loop");
       let box = document.createElement("div");
