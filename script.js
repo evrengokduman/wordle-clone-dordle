e4e0cd76889b0011c3e21c28476c309627dba24d
evrengokduman <sukruevrengokduman@gmail.com>
2023-05-09 23:22:40 +0300

    added insertLetter function

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ function initBoard() {
     console.log("first loop");
     let row = document.createElement("div");
     row.className = "letter-row";
-    //loop 5 times and creates boxes and it's gonna nest each of those boxes inside of row
+
     for (let j = 0; j < 5; j++) {
+      //this loop, each time it runs, it's gonna create a box with a class of letter-box
       console.log("second loop");
       let box = document.createElement("div");
       box.className = "letter-box";
@@ -28,3 +29,44 @@ function initBoard() {
 }
 console.log("done");
 initBoard();
+
+document.addEventListener("keyup", (e) => {
+  if (guessesRemaining === 0) {
+    return;
+  }
+
+  let pressedKey = String(e.key);
+  if (pressedKey === "Backspace" && nextLetter !== 0) {
+    //if you press backspace, it's gonna delete the letter
+    deleteLetter();
+    return;
+  }
+
+  if (pressedKey === "Enter") {
+    //if you press enter, it's gonna check the guess
+    checkGuess();
+    return;
+  }
+
+  let found = pressedKey.match(/[a-z]/gi);
+  if (!found || found.length > 1) {
+    return; //if you press a key that's not a letter, it's gonna return
+  } else {
+    insertLetter(pressedKey); //if you press a letter, it's gonna insert the letter
+  }
+});
+
+function insertLetter(pressedKey) {
+  if (nextLetter === 5) {
+    return;
+  }
+  pressedKey = pressedKey.toLowerCase();
+
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]; //this is the row that we're on
+
+  let box = row.children[nextLetter]; //this is the box that we're on
+  box.textContent = pressedKey;
+  box.classList.add("filled-box");
+  currentGuess.push(pressedKey);
+  nextLetter += 1;
+}
